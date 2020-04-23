53d9b52068329bd1a05995dfe5ea0695e918346a
_ <53088156+636f7374@users.noreply.github.com>
2020-04-24 06:52:05 +0800

    Improve: Preview Transport.

--- src/wrench/io.cr
+++ src/wrench/io.cr
@@ -8,17 +8,16 @@ abstract class IO
 
   # Warning: Inaccurate CopyException count. (Maybe)
 
-  def self.copy(src, dst, cause : Bool) : UInt64
-    return copy src, dst unless cause
-
-    buffer = uninitialized UInt8[16384_i32]
+  def self.super_copy(src : IO, dst : IO, &block : ->) : UInt64
+    buffer = uninitialized UInt8[4096_i32]
     count = 0_u64
 
     begin
       while (len = src.read(buffer.to_slice).to_i32) > 0_i32
         dst.write buffer.to_slice[0_i32, len]
-
         count += len
+
+        yield
       end
     rescue ex
       raise CopyException.new message: String.new, cause: ex, count: count
--- src/wrench/transport.cr
+++ src/wrench/transport.cr
@@ -6,14 +6,6 @@ class Transport
   def initialize(@client, @remote : IO, @callback : Proc(UInt64, UInt64, Nil)? = nil)
   end
 
-  def maximum_timed_out=(value : Int32)
-    @maximumTimedOut = value
-  end
-
-  def maximum_timed_out
-    @maximumTimedOut || 64_i32
-  end
-
   private def uploaded_size=(value : UInt64)
     @uploadedSize = value
   end
@@ -30,73 +22,66 @@ class Transport
     @receivedSize
   end
 
-  # I ’m going to make a long talk here, about why I did this (all_transport)
-  # Some time ago, I found that if the client is writing data to Remote, if Remote is also reading at the same time, it will trigger the problem of read timeout, and vice versa.
-  # So far, I still don't know if it is a bad implementation of Crystal (IO::Evented).
-  # So I thought of this solution.
-  # When the timeout is triggered, immediately check whether the other party (upload / receive) has completed the transmission, otherwise continue to loop IO.copy.
-  # At the same time, in order to avoid the infinite loop problem, I added the maximum number of attempts.
-  # This ensures that there is no disconnection when transferring data for a long time.
-  # Taking a 30-second timeout as an example, 30 * maximum number of attempts (default: 64) = 1920 seconds
+  private def last_alive=(value : Time)
+    @lastAlive = value
+  end
+
+  private def last_alive
+    @lastAlive
+  end
+
+  def alive_interval=(value : Time::Span)
+    @aliveInterval = value
+  end
+
+  def alive_interval
+    @aliveInterval || 3_i32.minutes
+  end
 
   def perform
+    self.last_alive = Time.local
+
     spawn do
-      timed_out_counter = 0_u64
       exception = nil
       count = 0_u64
 
       loop do
         size = begin
-          IO.copy client, remote, true
+          IO.super_copy(client, remote) { self.last_alive = Time.local }
         rescue ex : IO::CopyException
           exception = ex.cause
           ex.count
-        rescue
-          nil
         end
 
         size.try { |_size| count += _size }
-        break if maximum_timed_out <= timed_out_counter
 
-        case exception
-        when IO::TimeoutError
-          timed_out_counter += 1_i32
-          next sleep 0.05_f32.seconds unless received_size
-        else
-        end
-
-        break
+        break unless _last_alive = last_alive
+        break if (Time.local - _last_alive) > alive_interval
+        break if exception.nil?
+        break unless exception.is_a? IO::TimeoutError
       end
 
       self.uploaded_size = count || 0_u64
     end
 
     spawn do
-      timed_out_counter = 0_u64
       exception = nil
       count = 0_u64
 
       loop do
         size = begin
-          IO.copy remote, client, true
+          IO.super_copy(remote, client) { self.last_alive = Time.local }
         rescue ex : IO::CopyException
           exception = ex.cause
           ex.count
-        rescue ex
-          nil
         end
 
         size.try { |_size| count += _size }
-        break if maximum_timed_out <= timed_out_counter
 
-        case exception
-        when IO::TimeoutError
-          timed_out_counter += 1_i32
-          next sleep 0.05_f32.seconds unless uploaded_size
-        else
-        end
-
-        break
+        break unless _last_alive = last_alive
+        break if (Time.local - _last_alive) > alive_interval
+        break if exception.nil?
+        break unless exception.is_a? IO::TimeoutError
       end
 
       self.received_size = count || 0_u64
