a8a22bdadee21c12e485ef6e779a28ade86a5042
_ <53088156+636f7374@users.noreply.github.com>
2020-12-01 12:45:12 +0800

    Fix the problem of high CPU usage caused by Transport.

--- src/wrench/transport.cr
+++ src/wrench/transport.cr
@@ -137,11 +137,13 @@ class Transport
 
         break unless _last_alive = last_alive
         break if (Time.local - _last_alive) > alive_interval
+
         break if received_size && exception
         break unless exception
-
         next if exception.is_a? IO::TimeoutError
         break if exception.is_a? IO::Error
+
+        sleep 0.05_f32
       end
 
       self.uploaded_size = (count || 0_i64) + extra_uploaded_size
@@ -163,11 +165,13 @@ class Transport
 
         break unless _last_alive = last_alive
         break if (Time.local - _last_alive) > alive_interval
+
         break if uploaded_size && exception
         break unless exception
-
         next if exception.is_a? IO::TimeoutError
         break if exception.is_a? IO::Error
+
+        sleep 0.05_f32
       end
 
       self.received_size = (count || 0_i64) + extra_received_size
