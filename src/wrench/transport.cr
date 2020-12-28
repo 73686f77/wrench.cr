eda1e25aa8dab1c8df9bb4112c7f3f0a848d2162
_ <53088156+636f7374@users.noreply.github.com>
2020-12-28 15:46:00 +0800

    Breaking change: Optimize Transport.
    I. When performing cleanup, first close the socket connection, and then wait for the light to die before free the TLS context.
    
    II. Reliable replaces Side to distinguish when one party is disconnected or when both parties are disconnected.

--- src/wrench/transport.cr
+++ src/wrench/transport.cr
@@ -4,14 +4,21 @@ class Transport
     Remote = 1_u8
   end
 
+  enum Reliable : UInt8
+    Half = 0_u8
+    Full = 1_u8
+  end
+
   getter client : IO
   getter remote : IO
   getter callback : Proc(Int64, Int64, Nil)?
   getter heartbeat : Proc(Nil)?
   getter mutex : Mutex
+  getter workerFibers : Array(Fiber)
 
   def initialize(@client, @remote : IO, @callback : Proc(Int64, Int64, Nil)? = nil, @heartbeat : Proc(Nil)? = nil)
     @mutex = Mutex.new :unchecked
+    @workerFibers = [] of Fiber
   end
 
   def remote_tls_context=(value : OpenSSL::SSL::Context::Client)
@@ -102,36 +109,87 @@ class Transport
     @extraReceivedSize || 0_i32
   end
 
-  def side=(side : Side)
-    @side = side
+  def reliable=(reliable : Reliable)
+    @reliable = reliable
   end
 
-  def side
-    @side
+  def reliable
+    @reliable
   end
 
-  def cleanup
-    unless remote.closed?
-      remote.close rescue nil
-      remote_tls.try &.free
-      remote_tls_context.try &.free
+  def finished?
+    dead_count = @mutex.synchronize { workerFibers.count { |fiber| fiber.dead? } }
+    all_task_size = @mutex.synchronize { workerFibers.size }
+
+    dead_count == all_task_size
+  end
+
+  def cleanup_all
+    client.close rescue nil
+    remote.close rescue nil
+
+    loop do
+      finished = self.finished?
+
+      if finished
+        free_client_tls
+        free_remote_tls
+
+        break
+      end
+
+      sleep 0.25_f32
     end
+  end
 
-    unless client.closed?
+  def cleanup_side(side : Side, free_tls : Bool)
+    case side
+    in .client?
       client.close rescue nil
-      client_tls.try &.free
-      client_tls_context.try &.free
+    in .remote?
+      remote.close rescue nil
     end
+
+    loop do
+      finished = self.finished?
+
+      if finished
+        case side
+        in .client?
+          free_client_tls
+        in .remote?
+          free_remote_tls
+        end
+
+        break
+      end
+
+      sleep 0.25_f32
+    end
+  end
+
+  def free_client_tls
+    client_tls.try &.free
+    client_tls_context.try &.free
+  end
+
+  def free_remote_tls
+    remote_tls.try &.free
+    remote_tls_context.try &.free
   end
 
   def update_latest_alive
     @mutex.synchronize { @latestAlive = Time.local }
   end
 
+  def add_worker_fiber(fiber : Fiber)
+    @mutex.synchronize { @workerFibers << fiber }
+  end
+
   def perform
     update_latest_alive
 
-    spawn do
+    upload_fiber = spawn do
       exception = nil
       count = 0_i64
 
@@ -149,16 +207,14 @@ class Transport
         break if alive_interval <= (Time.local - _latest_alive)
 
         break if received_size && exception
-        break unless exception
-        break if exception.is_a? IO::Error
-
-        sleep 0.05_f32.seconds
+        next sleep 0.05_f32.seconds if exception.is_a? IO::TimeoutError
+        break
       end
 
       self.uploaded_size = (count || 0_i64) + extra_uploaded_size
     end
 
-    spawn do
+    receive_fiber = spawn do
       exception = nil
       count = 0_i64
 
@@ -176,20 +232,18 @@ class Transport
         break if alive_interval <= (Time.local - _latest_alive)
 
         break if uploaded_size && exception
-        break unless exception
-        break if exception.is_a? IO::Error
-
-        sleep 0.05_f32.seconds
+        next sleep 0.05_f32.seconds if exception.is_a? IO::TimeoutError
+        break
       end
 
       self.received_size = (count || 0_i64) + extra_received_size
     end
 
-    spawn do
+    mixed_fiber = spawn do
       loop do
         status = ->do
-          case side
-          when Side::Client
+          case reliable
+          when Reliable::Half
             uploaded_size || received_size
           else
             uploaded_size && received_size
@@ -198,26 +252,24 @@ class Transport
 
         if status.call
           loop do
-            _uploaded_size = uploaded_size
-            _received_size = received_size
+            next sleep 0.05_f32.seconds unless _uploaded_size = uploaded_size
+            next sleep 0.05_f32.seconds unless _received_size = received_size
 
-            if _uploaded_size && _received_size
-              break callback.try &.call _uploaded_size, _received_size
-            end
-
-            sleep 0.05_f32.seconds
+            break callback.try &.call _uploaded_size, _received_size
           end
 
           break
         end
 
-        if _heartbeat = heartbeat
-          _heartbeat.call rescue nil
-          sleep heartbeat_interval.seconds
-        else
-          sleep 0.25_f32.seconds
-        end
+        next sleep 0.25_f32.seconds unless heartbeat
+
+        heartbeat.try &.call rescue nil
+        sleep heartbeat_interval.seconds
       end
     end
+
+    add_worker_fiber upload_fiber
+    add_worker_fiber receive_fiber
+    add_worker_fiber mixed_fiber
   end
 end
